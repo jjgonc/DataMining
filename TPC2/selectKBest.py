de44853e942ae23f95dbbb3868279bcb663b4350
jjgonc <josejoao_2001@hotmail.com>
2023-03-10 11:43:09 +0000

    selectKBest done

--- TPC2/selectKBest.py
+++ TPC2/selectKBest.py
@@ -5,43 +5,6 @@ import sys
 sys.path.append('../TPC1')
 from tpc1 import Dataset
 
-# class SelectKBest:
-#     def __init__(self, score_func: Callable = f_classif, k: int=10):
-#         if k <= 0:
-#             raise ValueError("k must be positive")
-        
-#         self.k = k
-#         self.p = None
-#         self.F = None
-#         self.score_func = score_func
-        
-
-#     def fit(self, dataset: Dataset) -> 'SelectKBest':
-#         self.F, self.p = self.score_func(dataset)
-#         return self
-
-#     def transform(self, dataset: Dataset) -> Dataset:
-#         indexes = np.argsort(self.F)[-self.k:]
-#         features = np.array(dataset.getFeatureNames())[indexes]
-#         return Dataset(x=dataset.getX()[:, indexes], y=dataset.getY(), feature_names=list(features), label_names=dataset.getLabelNames())
-
-#     def fit_transform(self, dataset: Dataset) -> Dataset:
-#         self.fit(dataset)
-#         return self.transform(dataset)
-    
-# if __name__ == "__main__":
-#     x = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-#     y = np.array([0, 0, 0])
-
-#     ds = Dataset(x, y, feature_names=['a', 'b', 'c'], label_names=['d'])
-
-#     sb = SelectKBest(score_func=f_classif, k=2)
-#     res = sb.fit_transform(ds)
-#     print(res.getLabelNames())
-
-
-import numpy as np
-
 class SelectKBest:
     def __init__(self, score_func, k):
         self.score_func = score_func
