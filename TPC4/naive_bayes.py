ebc436f351f7e0e75c98c4a1cd77985a79bfac46
Rui Guilherme Monteiro <61871927+rushmetra@users.noreply.github.com>
2023-03-31 11:23:19 +0100

    Add documentation

--- TPC4/naive_bayes.py
+++ TPC4/naive_bayes.py
@@ -1,12 +1,35 @@
 import numpy as np
 
 class NaiveBayesClassifier:
+    """
+    Classificador Naive Bayes para classificação de frases.
+
+    Atributos:
+    - classes (numpy.ndarray): array com as classes do modelo.
+    - class_prob (numpy.ndarray): array com as probabilidades das classes.
+    - vocab (numpy.ndarray): array com o vocabulário utilizado pelo vetorizador.
+
+    Métodos:
+    - fit(X_train, y_train): ajusta o modelo aos dados de treinamento.
+    - predict(X_test): classifica as frases de teste usando o modelo ajustado.
+    """
+
     def __init__(self):
+        """
+        Inicializa um objeto da classe NaiveBayesClassifier.
+        """
         self.classes = None
         self.class_prob = None
         self.vocab = None
 
     def fit(self, X_train, y_train):
+        """
+        Ajusta o modelo aos dados de treinamento.
+
+        Argumentos:
+        - X_train (numpy.ndarray): matriz de vetores de palavras de treinamento.
+        - y_train (numpy.ndarray): array com as classes das frases de treinamento.
+        """
         self.classes = np.unique(y_train)
         self.means = []
         self.variances = []
@@ -17,7 +40,16 @@ class NaiveBayesClassifier:
             self.variances.append(np.var(X_class, axis=0))
 
     def predict(self, X_train):
-        y_pred = []           # FIXME
+        """
+        Classifica as frases de teste usando o modelo ajustado.
+
+        Argumentos:
+        - X_test (numpy.ndarray): matriz de vetores de palavras de teste.
+
+        Retorna:
+        - y_pred (numpy.ndarray): array com as classes preditas das frases de teste.
+        """
+        y_pred = []           
         for x in X_train:
             class_prob = []
             for i, c in enumerate(self.classes):
@@ -31,7 +63,10 @@ class NaiveBayesClassifier:
 
 
 def main():
-    # Load data
+    '''
+    Chama as outras funções, cria e ajusta o classificador e calcula a acurácia do modelo na classificação das frases de teste.
+    '''
+    # Carregar dados
     X_train, y_train, X_test, y_test = load_data()
 
     # Criar e ajustar o classificador Naive Bayes
@@ -50,10 +85,19 @@ def main():
     print('Predictions:',y_pred)
 
 def load_data():
+    """
+    Carrega os dados de treinamento e teste, cria o vetorizador e transforma as frases em matrizes numéricas.
+
+    Retorna:
+    - X_train (numpy.ndarray): matriz de vetores de palavras de treinamento.
+    - y_train (numpy.ndarray): array com as classes das frases de treinamento.
+    - X_test (numpy.ndarray): matriz de vetores de palavras de teste.
+    - y_test (numpy.ndarray): array com as classes das frases de teste.
+    """
     # Criar um vetorizador para converter as frases em matriz numérica
     vectorizer = CountVectorizer()
 
-    # Load data from files or elsewhere
+    # Carregar dados
     train_sentences = ["a baixa do porto", "o mercado do bolhão é no porto", "a baixa de lisboa","o estadio é em lisboa","a rua de santa catarina fica no porto"]
     test_sentences = ["a câmara do porto fica no centro do porto", "o casino de lisboa","o hotel do porto","alvalade é em lisboa","aquela loja fica no porto"]
 
