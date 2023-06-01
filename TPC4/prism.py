e36b0a3783c6ad27cbc97f00c1e6c2b78103c05a
Rui Guilherme Monteiro <61871927+rushmetra@users.noreply.github.com>
2023-06-01 13:32:04 +0100

    New prism
    Usa o dataset das flores

--- TPC4/prism.py
+++ TPC4/prism.py
@@ -1,81 +1,178 @@
-import pandas as pd
 import numpy as np
-from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from collections import Counter
 
-class Prism:
-    def __init__(self, min_support=0.1, max_rules=10):
-        self.min_support = min_support
+class PRISMClassifier:
+    def __init__(self, max_rules=10):
         self.max_rules = max_rules
         self.rules = []
 
-    def fit(self, X, y):
-        self.X = X
-        self.y = y
-        df = pd.concat([X, y], axis=1)
-        for column in X.columns:
-            df[column] = pd.qcut(df[column], q=10, duplicates='drop')
+    def fit(self, X_train, y_train):
+        """
+        Fit the PRISM classifier to the training data.
 
-        self.rules = self._prism(df)
+        Parameters:
+            X_train (ndarray): Training features.
+            y_train (ndarray): Training labels.
+
+        Returns:
+            None
+        """
+        while X_train.shape[0] > 0 and len(self.rules) < self.max_rules:
+            best_rule = self.get_best_rule(X_train, y_train)
+            if best_rule is None:
+                break
+            self.rules.append(best_rule)
+
+            X_train, y_train = self.remove_patterns(X_train, y_train, best_rule)
+
+    def get_best_rule(self, X_train, y_train):
+        """
+        Find the best rule for the PRISM classifier based on the training data.
+
+        Parameters:
+            X_train (ndarray): Training features.
+            y_train (ndarray): Training labels.
+
+        Returns:
+            best_rule (tuple): The best rule found.
+        """
+        best_rule = None
+        max_accuracy = -10
+
+        for feature_index in range(X_train.shape[1]):
+            feature_values = np.unique(X_train[:, feature_index])
+            for feature_value in feature_values:
+                for class_label in np.unique(y_train):
+                    rule = (feature_index, feature_value, class_label)
+                    accuracy = self.evaluate_pattern(X_train, y_train, rule)
+                    if accuracy > max_accuracy:
+                        best_rule = rule
+                        max_accuracy = accuracy
+
+        return best_rule
+
+    def evaluate_pattern(self, X, y, pattern):
+        """
+        Evaluate the accuracy of a rule/pattern on the given data.
+
+        Parameters:
+            X (ndarray): Input features.
+            y (ndarray): Input labels.
+            pattern (tuple): The rule/pattern to evaluate.
+
+        Returns:
+            accuracy (float): The accuracy of the rule/pattern.
+        """
+        pattern_index, pattern_value, pattern_class = pattern
+        matches_pattern = X[:, pattern_index] == pattern_value
+        return np.mean(y[matches_pattern] == pattern_class)
+
+    def remove_patterns(self, X, y, pattern):
+        """
+        Remove the instances covered by a rule/pattern from the data.
+
+        Parameters:
+            X (ndarray): Input features.
+            y (ndarray): Input labels.
+            pattern (tuple): The rule/pattern to remove.
+
+        Returns:
+            X_updated (ndarray): Updated features after removing covered instances.
+            y_updated (ndarray): Updated labels after removing covered instances.
+        """
+        pattern_index, pattern_value, _ = pattern
+        uncovered = X[:, pattern_index] != pattern_value
+        return X[uncovered], y[uncovered]
 
     def predict(self, X):
-        y_pred = []
-        default_class = pd.Series(self.y).mode().values[0]
-        for _, row in X.iterrows():
+        """
+        Predict the labels for the input data.
+
+        Parameters:
+            X (ndarray): Input features.
+
+        Returns:
+            predictions (list): Predicted labels for the input data.
+        """
+        predictions = []
+        for instance in X:
+            instance_predictions = []
             for rule in self.rules:
-                if all([row[k] in v for k, v in rule['conditions'].items()]):
-                    y_pred.append(rule['class'])
-                    break
+                feature_index, feature_value, class_label = rule
+                if instance[feature_index] == feature_value:
+                    instance_predictions.append(class_label)
+            if instance_predictions:
+                class_counts = Counter(instance_predictions)
+                most_common_class = class_counts.most_common(1)[0][0]
+                predictions.append(most_common_class)
             else:
-                y_pred.append(default_class)
-        return y_pred
+                predictions.append(None)
+        return predictions
 
-    def _prism(self, df):
-        rules = []
-        while len(rules) < self.max_rules:
-            candidates = []
-            for column in df.columns[:-1]:
-                for value in df[column].unique():
-                    df_subset = df[df[column] == value]
-                    support = df_subset.iloc[:, -1].mean()
-                    if support >= self.min_support:
-                        conditions = {column: [value]}
-                        class_ = df_subset.iloc[:, -1].mode().values[0]
-                        candidates.append({'conditions': conditions, 'class': class_, 'support': support})
+    def accuracy_score(self, y_true, y_pred):
+        """
+        Compute the accuracy score.
 
-            if not candidates:
-                break
+        Parameters:
+            y_true (ndarray): True labels.
+            y_pred (ndarray): Predicted labels.
 
-            candidates.sort(key=lambda x: x['support'], reverse=True)
-            rule = candidates[0]
-            rules.append(rule)
-            df = df[~((df[rule['conditions'].keys()] == rule['conditions']).all(axis=1))]
-        
-        self.rules = rules  # Save the rules as an instance attribute
-        return rules
-    
+        Returns:
+            accuracy (float): Accuracy score.
+        """
+        return np.mean(y_true == y_pred)
+
     def __repr__(self):
-        repr_str = "PRISM Rules:\n"
+        """
+        Generate a string representation of the PRISM classifier rules.
+
+        Returns:
+            repr_str (str): String representation of the classifier rules.
+        """
+        repr_str = "PRISM Classifier Rules:\n"
         for i, rule in enumerate(self.rules):
-            repr_str += f"Rule {i+1}: Class={rule['class']}, Support={rule['support']}, Conditions={rule['conditions']}\n"
+            repr_str += f"Rule {i+1}: {rule}\n"
         return repr_str
 
 
+def test():
+    # Training data
+    X_train = np.array([
+        ['setosa', 'versicolor', 'virginica'],
+        ['versicolor', 'setosa', 'virginica'],
+        ['setosa', 'virginica', 'versicolor'],
+        ['setosa', 'setosa', 'virginica'],
+        ['virginica', 'setosa', 'versicolor'],
+        ['versicolor', 'versicolor', 'setosa'],
+        ['setosa', 'versicolor', 'virginica'],
+        ['setosa', 'setosa', 'virginica'],
+        ['virginica', 'virginica', 'setosa'],
+        ['versicolor', 'setosa', 'virginica']
+    ])
+    y_train = np.array(['class1', 'class2', 'class2', 'class1', 'class2', 'class1', 'class2', 'class1', 'class2', 'class2'])
 
+    # Test data
+    X_test = np.array([
+        ['setosa', 'versicolor', 'virginica'],
+        ['setosa', 'virginica', 'versicolor'],
+        ['versicolor', 'setosa', 'virginica'],
+        ['virginica', 'setosa', 'versicolor'],
+        ['setosa', 'setosa', 'virginica'],
+        ['virginica', 'virginica', 'setosa']
+    ])
+    y_test = np.array(['class2', 'class2', 'class1', 'class2', 'class1', 'class1'])
 
-def main():
-    iris = load_iris()
-    X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2, random_state=42)
+    prism = PRISMClassifier(max_rules=10)  # Set the maximum number of rules to 10
+    prism.fit(X_train, y_train)
+    print(prism)
 
-    prism = Prism(min_support=0.1, max_rules=10)
-    prism.fit(pd.DataFrame(X_train, columns=iris.feature_names), pd.Series(y_train))
+    predictions = prism.predict(X_test)
+    print("Predictions:", predictions)
+    print("True labels:", y_test)
 
-    print(prism)  # Imprime as regras geradas pelo algoritmo PRISM
+    accuracy = prism.accuracy_score(y_test, predictions)
+    print("Accuracy:", accuracy)
 
-    y_pred = prism.predict(pd.DataFrame(X_test, columns=iris.feature_names))
-
-    print(f"Accuracy: {accuracy_score(y_test, y_pred)}")
 
 if __name__ == "__main__":
-    main()
+    test()
